30b3ffe01fcc83db885565502f8e01c11ec12d4d
jojoisthebestpilotever30 <oscar.mcnicholl@my.rongotai.school.nz>
2026-08-27 23:55:09 +0000

    save

--- includes/carousel.php
+++ includes/carousel.php
@@ -1,12 +1,13 @@
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://www.movieposters.com/cdn/shop/files/the-odyssey_86fuoi4y.jpg?v=1779217171&width=250" class="d-block w-35" alt="The Odessey">
+      <img src="https://www.movieposters.com/cdn/shop/files/the-odyssey_86fuoi4y.jpg?v=1779217171&width=250" class="d-block w-35" alt="The Odyssey">
     </div>
     <div class="carousel-item">
-      <img src="https://www.sasktel.com/images/tv/ondemand/posters/2025-09-jurassicworldrebirth-250x375.jpg" class="d-block w-35" alt="Jurassic World:Rebirth">
+      <img src="https://www.sasktel.com/images/tv/ondemand/posters/2025-09-jurassicworldrebirth-250x375.jpg" class="d-block w-35" alt="Jurassic World: Rebirth">
     </div>
-    <div class="carousel-item"> 
+    <div class="carousel-item">
       <img src="https://d21ehp1kf1k9m9.cloudfront.net/wp-content/uploads/2024/11/14145044/Nosferatu-Poster-250x375.jpg" class="d-block w-35" alt="Nosferatu">
     </div>  
+  </div>
 </div>
