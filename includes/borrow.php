874cc5cab9da49f51306ad518417802eeb3c99f9
jojoisthebestpilotever30 <oscar.mcnicholl@my.rongotai.school.nz>
2026-08-28 00:08:46 +0000

    save

--- includes/borrow.php
+++ includes/borrow.php
@@ -2,7 +2,7 @@
 session_start();
 require('includes/auth_check.php');
 
-$page_title = "Log a loan | Gear Out";
+$page_title = "Log a loan | Movieloans";
 
 // If save_loan.php redirected back here with errors, read them once.
 $errors = $_SESSION['borrow_errors'] ?? [];
